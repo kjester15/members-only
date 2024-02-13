525096bfd6d5ce447b513d97cf7bd83cfb74cf77
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2024-02-12 17:56:26 -0800

    Add before_action to posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,12 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:new, :create]
 
   def new
   end
 
   def create
+  end
+
+  def index
   end
 end
