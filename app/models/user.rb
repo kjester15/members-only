7a8ac4ee6490eeb6a83f27fa5cfb353246a7a21b
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2024-02-12 17:48:29 -0800

    Remove validations from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :username, presence: true
-  validates :email, presence: true
-  validates :password, presence: true
 end
