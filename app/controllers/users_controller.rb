c6ffb0d4c5c66cc446a93a73fce6dac515e87185
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2024-02-12 17:08:11 -0800

    Add standard #new and #create methods to User and create new user view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,23 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
