9336d65d79617f4ea20cd30a3f4f89b0602fe409
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2024-02-13 15:46:25 -0800

    Add devise views and delete old user view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,21 @@
-class UsersController < ApplicationController
-  def new
-    @user = User.new
-  end
+# class UsersController < ApplicationController
+#   def new
+#     @user = User.new
+#   end
 
-  def create
-    @user = User.new(user_params)
+#   def create
+#     @user = User.new(user_params)
 
-    if @user.save
-      redirect_to new_user_path
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
+#     if @user.save
+#       redirect_to new_user_path
+#     else
+#       render :new, status: :unprocessable_entity
+#     end
+#   end
 
-  private
+#   private
 
-  def user_params
-    params.require(:user).permit(:username, :email, :password)
-  end
-end
+#   def user_params
+#     params.require(:user).permit(:username, :email, :password)
+#   end
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users
   resources :posts, only: [:new, :create, :index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
