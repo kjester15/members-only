0cf20ba70795c8102ed5c3eba026439a90dbed54
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2024-02-13 17:44:52 -0800

    Roll back post migration and recreate post model, and merge user_id into post#create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,8 +6,22 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.new(post_params.merge({ user_id: current_user.id }))
+
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def index
+    @posts = Post.all
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_13_005419) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_14_011543) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
-    t.string "body"
+    t.text "body"
+    t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,4 +32,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_13_005419) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
